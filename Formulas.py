1726ff163d076fdb386b1330999fd5c648691fb3
ibrokhimcoder <50892377+ibrokhimcoder@users.noreply.github.com>
2026-05-19 15:21:38 -0400

    Hello Robert, these are some of the code that i've worked on. The formulas.py contain the formula that we've made. The mechanics.py is basically the code that sets our components up.

---
+++ Formulas.py
@@ -0,0 +1,22 @@
+import Math
+class Formulas:
+    g = 9.81
+    def findAngle(v, x, h, high):
+        part1 = ((v*v) + math.sqrt((v^4-g*(g*x^2-2*h*v^2))))/(g*x)
+        part2 = ((v*v) - (v*v) + math.sqrt((v^4-g*(g*x^2-2*h*v^2))))/(g*x)
+        ans1 = math.atan(part1)
+        ans2 = math.atan(part2)
+        if(ans1>ans2):
+            if(high):
+                return ans1
+            else:
+                return ans2
+        else:
+            if (high):
+                return ans2
+            else:
+                return ans1
+    def radToDegree(theta):
+        return theta/math.pi * 180
+
+
---
+++ Mechanics.py
@@ -0,0 +1,35 @@
+#We will be using the gpiozero library for rasberry pi
+from gpiozero import DistanceSensor
+from gpiozero import Motor
+from gpiozero import Stepper
+from time import sleep
+
+
+#This is the set-up code for the distance sensor
+dPin1 = 23;
+dPin2 = 24;
+sensor = DistanceSensor(dpin1, dpin2)
+distance = sensor.distance()
+#----------------------------------------
+
+#This is the set-up code for the DC motor
+forward = 4;
+backward = 5;
+motor = Motor(forward, backward);
+#motor.forward() --> moves the motor forward
+#motor.backward() --> moves the motor backward
+#------------------------------------------------------
+
+#This is the set-up code for the stepper motor
+sPin1 = 1;
+sPin2 = 2;
+sPin3 = 3;
+sPin4 = 4;
+steps = 2048;
+speed = 10;
+step_motor = Stepper(steps, sPin1, sPin2, sPin3, sPin4)
+step_motor.set_speed(speed);
+#step_motor.step(1024) rotates the stepper motor halfway
+#step_motor.step(-1024) rotates the stepper motor halfway in the other direction
+
+
