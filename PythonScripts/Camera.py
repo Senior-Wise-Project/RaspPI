5a7e4d048e1acf06a82ac2ad3af3bc956291e72d
Robert Zamora <RobertZamora.act@gmail.com>
2026-05-20 15:38:58 -0400

    Small update
    small update

--- PythonScripts/Camera.py
+++ PythonScripts/Camera.py
@@ -1,8 +1,8 @@
 from time import sleep
-from picamera2 import PiCamera2
+from picamera2 import Picamera2
 from pathlib import Path
 
-camera = PiCamera2()
+camera = Picamera2()
 #-----------------------------------
 # What you write:
     #camera = picamera.PiCamera()
@@ -16,7 +16,7 @@ camera.start()
 # Give the sensor 2 seconds to adjust exposure, white balance, and focus
 sleep(2)
 imagePath = Path.cwd().parent / Path('Images') / Path('image.jpg')
-camera.capture_file("my_image.jpg")
+camera.capture_file(imagePath)
 camera.stop()
 print("Image captured successfully!")
 #the camera finishes adjusting during these 2 seconds
