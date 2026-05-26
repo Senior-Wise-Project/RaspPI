fe2ff3ece67a5fe841e6d87e1f0ddd300bcbfa13
ibrokhimcoder <50892377+ibrokhimcoder@users.noreply.github.com>
2026-05-26 12:33:37 -0400

    Make sure the image folder is where it is supposed to be

--- PythonScripts/Mechanics.py
+++ PythonScripts/Mechanics.py
@@ -5,8 +5,8 @@ from PythonScripts.Stepper import Stepper
 
 
 #This is the set-up code for the distance sensor
-dPin1 = 23;
-dPin2 = 24;
+dPin1 = 5;
+dPin2 = 4;
 sensor = DistanceSensor(dPin1, dPin2)
 distance = sensor.distance()
 #----------------------------------------
@@ -20,14 +20,22 @@ motor = Motor(forward, backward);
 #------------------------------------------------------
 
 #This is the set-up code for the stepper motor
-sPin1 = 1;
-sPin2 = 2;
-sPin3 = 3;
-sPin4 = 4;
+sPin1 = 16;
+sPin2 = 12;
+sPin3 = 6;
+sPin4 = 13;
 steps = 2048;
 speed = 10;
 step_motor = Stepper(steps, sPin1, sPin2, sPin3, sPin4)
 step_motor.set_speed(speed);
+s2Pin1 = 17;
+s2Pin2 = 27;
+s2Pin3 = 22;
+s2Pin4 = 23;
+steps = 2048;
+speed = 10;
+step_motor2 = Stepper(steps, s2Pin1, s2Pin2, s2Pin3, s2Pin4)
+step_motor2.set_speed(speed);
 #step_motor.step(1024) rotates the stepper motor halfway
 #step_motor.step(-1024) rotates the stepper motor halfway in the other direction
 
